af2e1f195fe7775058f294e781d88d741ad2d271
June Shaw <june.shaw@me.com>
2015-12-07 10:43:18 -0700

    initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -8,4 +8,41 @@
 // added and displayed.  Example:  a new add button would change all
 // to display: block in the existing dom and add a new random fox which is
 // also display: block.  Hit the display float button and they all change
-// back to float and it adds a new floated random fox. 
+// back to float and it adds a new floated random fox.
+
+$('#trigger').click(function () {
+    genFoxes();
+    $('#container')
+  }
+)
+
+$('#remFoxes').click(function () {
+    $('#container').empty();
+  }
+)
+
+$('#addFloatFox').click(function () {
+    genFoxes();
+    $('#container img').css('display', 'inline-block');
+    // $('#container').style.display = 'inline-block';
+  }
+)
+
+$('#addBlockFox').click(function () {
+    genFoxes();
+    $('#container img').css('display', 'block');
+  }
+)
+
+function genFoxes() {
+  var foxStringName = genFoxFileName();
+  var img = document.createElement("img");
+  img.src=foxStringName;
+  $('#container').append(img);
+}
+
+function genFoxFileName() {
+  var randd = Math.floor((Math.random() * 3) );
+  var foxStringName = collection["images"][randd]["image_src"];
+  return (foxStringName);
+}
